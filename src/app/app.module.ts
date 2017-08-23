e8dca0eadde44f747885800bc29973af4a822729
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-23 18:17:25 +0700

    handle text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
 import { BookComponent } from './book/book.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
-    BookComponent
+    BookComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  name = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+  showEvent(event) {
+    this.name = event.target.value;
+  }
+}
