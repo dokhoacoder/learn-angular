c51727f170fcc513c20b5f467105be3adac290bd
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-28 15:58:42 +0700

    ngFor

--- src/app/struct/struct.component.ts
+++ src/app/struct/struct.component.ts
@@ -5,9 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './struct.component.html',
   styleUrls: ['./struct.component.css']
 })
+
 export class StructComponent implements OnInit {
   isShow = true;
-
+  arrSubjects = ['Angular', 'NodeJS', 'React'];
   constructor() { }
 
   ngOnInit() {
