b0a03046f701b55bdeed34a75dc9cfb8dcea04d3
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-28 15:41:54 +0700

    ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
 import { BookComponent } from './book/book.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { StructComponent } from './struct/struct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
     BookComponent,
-    UserFormComponent
+    UserFormComponent,
+    StructComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/struct/struct.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-struct',
+  templateUrl: './struct.component.html',
+  styleUrls: ['./struct.component.css']
+})
+export class StructComponent implements OnInit {
+  isShow = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
