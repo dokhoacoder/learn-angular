8b22fd2033423cb724a7502f88671d314e104b65
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-29 17:22:51 +0700

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BookComponent } from './book/book.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { StructComponent } from './struct/struct.component';
 import { WordsComponent } from './words/words.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { WordsComponent } from './words/words.component';
     BookComponent,
     UserFormComponent,
     StructComponent,
-    WordsComponent
+    WordsComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent implements OnInit {
+  @Input() name: String;
+  @Input() age: Number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
