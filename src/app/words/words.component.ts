f3498d462bfad74319188465ceb9645e5be109af
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-29 14:02:06 +0700

    remove words

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class WordsComponent implements OnInit {
   newEn = '';
   newVn = '';
+  isShowForm = false;
   arrWords = [
     { id: 1, en: 'action', vn: 'hành động', memorized: true },
     { id: 2, en: 'actor', vn: 'diễn viên', memorized: false },
@@ -29,5 +30,6 @@ export class WordsComponent implements OnInit {
     });
     this.newEn = '';
     this.newVn = '';
+    this.isShowForm = false;
   }
 }
