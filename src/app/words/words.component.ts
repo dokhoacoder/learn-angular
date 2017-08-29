8e4c69f31eb379aae7d9dd79ca207fbaa224fda3
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-29 16:29:23 +0700

    filter

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./words.component.css']
 })
 export class WordsComponent implements OnInit {
+  filterStatus = 'XEM_TAT_CA';
   newEn = '';
   newVn = '';
   isShowForm = false;
@@ -36,5 +37,12 @@ export class WordsComponent implements OnInit {
   removeWord(id: number) {
     const index = this.arrWords.findIndex(word => word.id === id);
     this.arrWords.splice(index, 1);
+  }
+
+  getShowStatus(memorized: boolean) {
+    const dkXemTatCa = this.filterStatus === 'XEM_TAT_CA';
+    const dkXemDaNho = this.filterStatus === 'XEM_DA_NHO' && memorized;
+    const dkXemChuaNho = this.filterStatus === 'XEM_CHUA_NHO' && !memorized;
+    return dkXemTatCa || dkXemDaNho || dkXemChuaNho;
   }
 }
