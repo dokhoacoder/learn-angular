645a9def24296dc6540c6b0b4d7178e2f5ee2546
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-30 16:17:28 +0700

    setup viewchild

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -2,20 +2,9 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-child',
-    template: `
-        <button (click)="addForParent();">Add</button>
-        <button (click)="subForParent();">Sub</button>
-    `
+    template: `<h3>{{ value }}</h3>`
 })
 
 export class ChildComponent {
-    @Output() myClick = new EventEmitter<boolean>();
-
-    addForParent() {
-        this.myClick.emit(true);
-    }
-
-    subForParent() {
-        this.myClick.emit(false);
-    }
+    value = 0;
 }
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -3,19 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-parent',
     template: `
-        <h3>{{ value }}</h3>
-        <app-child (myClick)="changeValue($event);"></app-child>
+        <button>Add for child</button>
+        <app-child></app-child>
     `
 })
 
-export class ParentComponent {
-    value = 0;
-
-    changeValue(isAdd: boolean) {
-        if (isAdd) {
-            this.value = this.value + 1;
-        } else {
-            this.value = this.value - 1;
-        }
-    }
-}
+export class ParentComponent {}
