567080db0889284a8c5c42fa1995932a87995053
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-07 14:51:22 +0700

    handle error and loading

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -11,16 +11,32 @@ export class WeatherComponent implements OnInit {
   txtCityName = '';
   cityName = '';
   temp = null;
+  isLoading = false;
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {}
 
   getWeather() {
+    this.isLoading = true;
     this.weatherService.getTemp(this.txtCityName)
     .then(temp => {
       this.cityName = this.txtCityName;
       this.temp = temp;
+      this.isLoading = false;
+      this.txtCityName = '';
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      alert('Cannot find your city!');
+      this.isLoading = false;
+      this.cityName = '';
+      this.txtCityName = '';
+    });
+  }
+
+  getMessage() {
+    if (this.isLoading) {
+      return 'Loading...';
+    }
+    return this.cityName === '' ? 'Enter your city name' : (this.cityName + ' is now ' + this.temp);
   }
 }
