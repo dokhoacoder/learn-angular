7a617d6535ee7ff531470f91347527bcfd505c6a
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-19 10:12:42 +0700

    nested form group

--- src/app/sign-up.component.ts
+++ src/app/sign-up.component.ts
@@ -9,6 +9,12 @@ import { FormGroup, FormControl } from '@angular/forms';
             <br><br>
             <input type="password" placeholder="Password" formControlName="password">
             <br><br>
+            <div formGroupName="subject">
+                <label><input type="checkbox" name="NodeJS" formControlName="nodeJS"> NodeJS</label>
+                <label><input type="checkbox" name="Angular" formControlName="angular"> Angular</label>
+                <label><input type="checkbox" name="ReactJS" formControlName="reactJS"> ReactJS</label>
+            </div>
+            <br><br>
             <button>Submit</button>
         </form>
     `
@@ -20,7 +26,12 @@ export class SignUpComponent {
     constructor() {
         this.formSignUp = new FormGroup({
             email: new FormControl(),
-            password: new FormControl
+            password: new FormControl(),
+            subject: new FormGroup({
+                nodeJS: new FormControl(),
+                angular: new FormControl(),
+                reactJS: new FormControl()
+            })
         });
     }
 
